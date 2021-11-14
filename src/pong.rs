b6c299519b13411e345dbc3847baff63bec375fe
Levente Krizsan <lewalevi@gmail.com>
2021-11-14 21:43:28 +0100

    feat(game): add 2d camera

--- src/pong.rs
+++ src/pong.rs
@@ -6,6 +6,28 @@ use amethyst::{
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
 };
 
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
 pub struct Pong;
 
-impl SimpleState for Pong {}
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        initialise_camera(world);
+    }
+}
+
+fn initialise_camera(world: &mut World) {
+    // setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        // create orthographic camera useful for 2D rendering, `z` is 1.0 as sprites are at `0.0`
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
