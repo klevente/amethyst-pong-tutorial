33a14a9365bdbfb995731f6d7ee7ef1e8f303305
Levente Krizsan <lewalevi@gmail.com>
2021-11-14 21:54:47 +0100

    feat(game): add paddles

--- src/pong.rs
+++ src/pong.rs
@@ -6,8 +6,38 @@ use amethyst::{
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
 };
 
-pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
+pub const ARENA_HEIGHT: f32 = 100.0;
+
+pub const PADDLE_WIDTH: f32 = 4.0;
+pub const PADDLE_HEIGHT: f32 = 16.0;
+
+#[derive(Eq, PartialEq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    fn new(side: Side) -> Self {
+        Self {
+            side,
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+impl Component for Paddle {
+    // specify the storage type of `Paddle`s, choosing the best requires benchmarking
+    type Storage = DenseVecStorage<Self>;
+}
 
 pub struct Pong;
 
@@ -15,6 +45,9 @@ impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
 
+        // components not used in any `System`s need to be manually registered in the `world`
+        world.register::<Paddle>();
+        initialise_paddles(world);
         initialise_camera(world);
     }
 }
@@ -29,5 +62,30 @@ fn initialise_camera(world: &mut World) {
         // create orthographic camera useful for 2D rendering, `z` is 1.0 as sprites are at `0.0`
         .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
         .with(transform)
+        .build();
+}
+
+// initialises one paddle on the left and one paddle on the right
+fn initialise_paddles(world: &mut World) {
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // paddles begin in the middle vertically
+    let y = ARENA_HEIGHT * 0.5;
+    // the anchor point of entities are their midpoints, this is why they have to be
+    // translated by half of their width in both cases
+    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
         .build();
 }
